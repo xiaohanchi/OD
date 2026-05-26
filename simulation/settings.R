769af0c48b8e34ca2a7b4b1ac662381a8e6d2403
xiaohanchi <xiaohanchi123@gmail.com>
2026-05-26 18:28:18 +0800

    add ar error code

--- simulation/settings.R
+++ simulation/settings.R
@@ -1,43 +1,10 @@
+# note: rho_source = c(0.3, 0.5) in case. prefer 0.5 but if coverage too low then -> 0.3
 beta_delta_setting <- matrix(
   c(0, 0, 0, 
     -2, 0, 0, 
     0, -2, 0), byrow = TRUE, nrow = 3
 )
 all.config <- bind_rows(
-  # consistent scenario under type 3
-  expand.grid(
-    model_type = c(1:5),
-    gmm_type = 1,
-    n_patients = 80,
-    beta_delta_idx = 1, 
-    rho_source = c(0.5, 0.8), 
-    re_factor = c(1), 
-    remote_inflation = 0,
-    outlier_rate1 = 0.2, # deprecated
-    outlier_rate2 = 0.5, # deprecated
-    outlier_type = 3,
-    error_type = 1, # iid
-    uni_modal = TRUE, # deprecated
-    high_freq = FALSE,
-    DGM_type = 1
-  ),
-  # gmm
-  expand.grid(
-    model_type = 6,
-    gmm_type = c(3:5),
-    n_patients = 80,
-    beta_delta_idx = 1, 
-    rho_source = c(0.5, 0.8), 
-    re_factor = c(1), 
-    remote_inflation = 0,
-    outlier_rate1 = 0.2,
-    outlier_rate2 = 0.5,
-    outlier_type = 3,
-    error_type = 1, # iid
-    uni_modal = TRUE,
-    high_freq = FALSE,
-    DGM_type = 1
-  ),
   # outlier 1: sporadic
   # rlmm
   expand.grid(
@@ -45,13 +12,13 @@ all.config <- bind_rows(
     gmm_type = 1,
     n_patients = 80,
     beta_delta_idx = 1:3, 
-    rho_source = c(0.5, 0.8), 
+    rho_source = c(0.3, 0.5), 
     re_factor = 2, # c(1, 2), 
     remote_inflation = 0,
     outlier_rate1 = 0.5,
     outlier_rate2 = 0.2,
     outlier_type = 1,
-    error_type = 1, # iid
+    error_type = 2, # AR
     uni_modal = TRUE,
     high_freq = FALSE,
     DGM_type = 1
@@ -62,13 +29,13 @@ all.config <- bind_rows(
     gmm_type = c(3:5),
     n_patients = 80,
     beta_delta_idx = 1:3, 
-    rho_source = c(0.5, 0.8), 
+    rho_source = c(0.3, 0.5), 
     re_factor = 2, # c(1, 2), 
     remote_inflation = 0,
     outlier_rate1 = 0.5,
     outlier_rate2 = 0.2,
     outlier_type = 1,
-    error_type = 1, # iid
+    error_type = 2, # AR
     uni_modal = TRUE,
     high_freq = FALSE,
     DGM_type = 1
@@ -80,13 +47,13 @@ all.config <- bind_rows(
     gmm_type = 1,
     n_patients = 80,
     beta_delta_idx = 1:3, 
-    rho_source = c(0.5, 0.8), 
+    rho_source = c(0.3, 0.5), 
     re_factor = 2, # c(1, 2), 
     remote_inflation = 0,
     outlier_rate1 = 0.2,
     outlier_rate2 = 0.5,
     outlier_type = 2,
-    error_type = 1, # iid
+    error_type = 2, # AR
     uni_modal = c(TRUE, FALSE),
     high_freq =  c(TRUE, FALSE),
     DGM_type = 1
@@ -97,13 +64,13 @@ all.config <- bind_rows(
     gmm_type = c(3:5),
     n_patients = 80,
     beta_delta_idx = 1:3, 
-    rho_source = c(0.5, 0.8), 
+    rho_source = c(0.3, 0.5), 
     re_factor = 2, # c(1, 2), 
     remote_inflation = 0,
     outlier_rate1 = 0.2,
     outlier_rate2 = 0.5,
     outlier_type = 2,
-    error_type = 1, # iid
+    error_type = 2, # AR
     uni_modal = c(TRUE, FALSE),
     high_freq = c(TRUE, FALSE),
     DGM_type = 1
@@ -115,13 +82,13 @@ all.config <- bind_rows(
     gmm_type = 1,
     n_patients = 80,
     beta_delta_idx = 1:3, 
-    rho_source = c(0.5, 0.8), 
+    rho_source = c(0.3, 0.5), 
     re_factor = 2, # c(1, 2), 
     remote_inflation = 0,
     outlier_rate1 = 0.2, # deprecated
     outlier_rate2 = 0.5, # deprecated
     outlier_type = 3,
-    error_type = 1, # iid
+    error_type = 2, # AR
     uni_modal = TRUE, # deprecated
     high_freq =c(TRUE, FALSE),
     DGM_type = 1
@@ -132,13 +99,13 @@ all.config <- bind_rows(
     gmm_type = c(3:5),
     n_patients = 80,
     beta_delta_idx = 1:3, 
-    rho_source = c(0.5, 0.8), 
+    rho_source = c(0.3, 0.5), 
     re_factor = 2, # c(1, 2), 
     remote_inflation = 0,
     outlier_rate1 = 0.2,
     outlier_rate2 = 0.5,
     outlier_type = 3,
-    error_type = 1, # iid
+    error_type = 2, # AR
     uni_modal = TRUE,
     high_freq = c(TRUE, FALSE),
     DGM_type = 1
