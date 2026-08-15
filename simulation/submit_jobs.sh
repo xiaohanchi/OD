0b1120e4edbc7d64034f0d7058aac4e08fbf9619
xiaohanchi <xiaohanchi123@gmail.com>
2026-08-15 12:00:19 -0500

    add ar code

--- simulation/submit_jobs.sh
+++ simulation/submit_jobs.sh
@@ -7,8 +7,8 @@ short_jobs=(1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26
 long_jobs=(179 180 182 183 185 186 188 189 191 192)
 run_jobs=(4 9 14 19 24 29 31 32 33 34 35 36 37 38 39 40 41 42 43 44 45 46 47 48 52 57 62 67 72 77 82 87 92 97 102 107 112 117 122 127 132 137 142 147 152 157 162 167 169 170 171 172 173 174 175 176 177 178 179 181 184 187 190 193 196 199 202 205 206 207 244 249 254 259 264 269 274 279 284 289 294 299)
 
-# for i in $(seq 1 ${n_jobs}); do
-for i in ${run_jobs[@]}; do
+for i in $(seq 1 ${n_jobs}); do
+# for i in ${run_jobs[@]}; do
 
 	mkdir -p jobtype$i/results
 	mkdir -p jobtype$i/raw_results
@@ -38,8 +38,8 @@ for s in $(seq 1 ${n_split}); do
 	start_idx=$(( (s - 1) * jobs_per_round + 1 ))
 	end_idx=$(( s * jobs_per_round ))
 	
-	# for i in $(seq 1 ${n_jobs}); do
-	for i in ${run_jobs[@]}; do
+	for i in $(seq 1 ${n_jobs}); do
+	# for i in ${run_jobs[@]}; do
 
 		bsub -J "outlier_${i}[${start_idx}-${end_idx}]" < ./jobtype$i/runjobs.lsf
 
